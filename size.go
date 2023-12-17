9d92dfff6b9462e3462c44ddc1caaef3b44b03df
246859 <2633565580@qq.com>
2023-12-17 16:50:21 +0800

    refactor(size): refactor size

--- size.go
+++ size.go
@@ -3,22 +3,26 @@ package size
 import (
 	"fmt"
 	"github.com/shopspring/decimal"
+	"regexp"
+	"slices"
 	"strconv"
-	"strings"
+	"unicode"
 )
 
-type Size = uint64
+type Unit = uint64
 
 const (
-	B  Size = 1
-	KB      = B * 1024
-	MB      = KB * 1024
-	GB      = MB * 1024
-	TB      = GB * 1024
-	PB      = TB * 1024
+	B Unit = 1 << (iota * 10)
+	KB
+	MB
+	GB
+	TB
+	PB
+	EB
 )
 
-var UnitMap = map[Size][]string{
+var unitsTable = map[Unit][]string{
+	EB: {"EB", "Eb", "eb", "eB", "E", "e"},
 	PB: {"PB", "Pb", "pb", "pB", "P", "p"},
 	TB: {"TB", "Tb", "tb", "tB", "T", "t"},
 	GB: {"GB", "Gb", "gb", "gB", "G", "g"},
@@ -27,73 +31,105 @@ var UnitMap = map[Size][]string{
 	B:  {"B", "b"},
 }
 
-type SizeMeta struct {
-	Data float64
-	Unit Size
-}
+// New returns a new size
+func New(size float64, unit Unit) Size {
+	if size < 0 {
+		size = -1
+	}
 
-func NewSize(size float64, unit Size) SizeMeta {
-	return SizeMeta{
+	return Size{
 		Data: size,
 		Unit: unit,
 	}
 }
 
-func (s SizeMeta) Round(n int32) float64 {
+// Size represents a bytes size in Unit
+type Size struct {
+	Data float64
+	Unit Unit
+}
+
+func (s Size) Round(n int32) float64 {
 	return decimal.NewFromFloat(s.Data).Round(n).InexactFloat64()
 }
 
-func (s SizeMeta) String() string {
-	if _, ok := UnitMap[s.Unit]; !ok {
-		return ""
+// StringRound returns the string representation of the size
+// if n < 0, use the original data as the float representation.
+func (s Size) StringRound(n int32) string {
+	if _, ok := unitsTable[s.Unit]; !ok {
+		return "unknown unit"
 	}
-	return fmt.Sprintf(`%.2f%s`, decimal.NewFromFloat(s.Data).Round(2).InexactFloat64(), UnitMap[s.Unit][0])
+	if n < 0 {
+		return fmt.Sprintf(`%.2f%s`, s.Data, unitsTable[s.Unit][0])
+	}
+	return fmt.Sprintf(`%.2f%s`, s.Round(n), unitsTable[s.Unit][0])
 }
 
-// ParseSize
-// @Date 2023-06-25 16:37:42
-// @Param str string
-// @Return SizeMeta
-// @Return error
-// @Description: 将字符串转换成对应的size描述
-func ParseSize(str string) SizeMeta {
-	var size SizeMeta
-	if len(str) == 0 {
-		return size
+// String By default, only three decimal places are retained
+func (s Size) String() string {
+	return s.StringRound(3)
+}
+
+func (s Size) To(to Unit) Size {
+	return Size{
+		Data: (s.Data * float64(s.Unit)) / float64(to),
+		Unit: to,
+	}
+}
+
+// match the string that starting with non-negative float and ending with letters
+var pattern = regexp.MustCompile("^\\d+(\\.\\d+)?[a-zA-Z]*$")
+
+// Lookup returns a new size from the given string, return false if parse failed,
+// use B unit if str has no unit string.
+func Lookup(str string) (Size, bool) {
+	var size Size
+	if len(str) == 0 || !pattern.MatchString(str) {
+		return size, false
 	}
 
-	for unit, aliasSlice := range UnitMap {
-		for _, alias := range aliasSlice {
-			if before, found := strings.CutSuffix(str, alias); found {
-				if float, err := strconv.ParseFloat(before, 64); err == nil {
-					return SizeMeta{
-						Data: float,
-						Unit: unit,
-					}
-				}
-			}
+	var (
+		floatStr string
+		unitStr  string
+	)
+
+	for i := len(str) - 1; i >= 0; i-- {
+		if !unicode.IsLetter(rune(str[i])) {
+			floatStr = str[:i+1]
+			unitStr = str[i+1:]
+			break
 		}
 	}
 
-	return size
+	if parseFloat, err := strconv.ParseFloat(floatStr, 64); err != nil {
+		return size, false
+	} else {
+		size.Data = parseFloat
+	}
+
+	// use the min unit if str has no unit,
+	if len(unitStr) == 0 {
+		size.Unit = B
+		return size, true
+	}
+
+	// find the unit
+	for unit, alias := range unitsTable {
+		if index := slices.Index(alias, unitStr); index != -1 {
+			size.Unit = unit
+			return size, true
+		}
+	}
+
+	// unit not found
+	return size, false
 }
 
-// ParseTargetSize
-// @Date 2023-06-25 16:43:06
-// @Param str string
-// @Param size Size
-// @Return SizeMeta
-// @Return error
-// @Description: 转换成指定的大小
-func ParseTargetSize(str string, size Size) SizeMeta {
-	parseSize := ParseSize(str)
-
-	bytes := parseSize.Data * float64(parseSize.Unit)
-
-	data := bytes / float64(size)
-
-	return SizeMeta{
-		Data: data,
-		Unit: size,
+// LookupTo returns a new size from the given string that convert to given unit, return false if failed
+func LookupTo(str string, to Unit) (Size, bool) {
+	parseSize, ok := Lookup(str)
+	if !ok {
+		return parseSize, ok
 	}
+	return parseSize.To(to), true
 }
